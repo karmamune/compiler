ec168c7bee4781d4d899a483980e9f95c2aa89d0
karmamune <manugarcia649@gmail.com>
2026-04-11 16:42:54 -0600

    Se continúan agregando las clases y métodos del capítulo 3

--- src/wci/frontend/pascal/PascalScanner.java
+++ src/wci/frontend/pascal/PascalScanner.java
@@ -1,7 +1,11 @@
 package wci.frontend.pascal;
 
 import wci.frontend.*;
+import wci.frontend.pascal.tokens.*;
+
 import static wci.frontend.Source.EOF;
+import static wci.frontend.pascal.PascalTokenType.*;
+import static wci.frontend.pascal.PascalErrorCode.*;
 
 /**
  * <h1>PascalScanner</h1>
@@ -19,20 +23,67 @@ public class PascalScanner extends Scanner {
         super(source);
     }
 
-
+    /**
+     * Extract and return the next Pascal token from the source.
+     * @return the next token.
+     * @throws Exception if an error occurred.
+     */
     protected Token extractToken()
         throws Exception
     {   
+        skipWhiteSpace();
+
         Token token;
         char currentChar = currentChar();
 
+        // Construct the next token.  The current character determines the
+        // token type.
         if (currentChar == EOF) {
             token = new EofToken(source);
         }
+        else if (Character.isLetter(currentChar)) {
+            token = new PascalWordToken(source);
+        }
+        else if (Character.isDigit(currentChar)) {
+            token = new PascalNumberToken(source);
+        }
+        else if (currentChar == '\'') {
+            token = new PascalStringToken(source);
+        }
+        else if (PascalTokenType.SPECIAL_SYMBOLS.containsKey(Character.toString(currentChar))) {
+            token = new PascalSpecialSymbolToken(source);
+        }
         else {
-            token = new Token(source);
+            token = new PascalErrorToken(source, INVALID_CHARACTER, Character.toString(currentChar));
+            nextChar();  // consume character
         }
 
         return token;
+    }
+
+
+    private void skipWhiteSpace()
+        throws Exception
+    {
+        char currentChar = currentChar();
+
+        while (Character.isWhitespace(currentChar) || (currentChar == '{')) {
+
+            // Start of a comment?
+            if (currentChar == '{') {
+                do {
+                    currentChar = nextChar();  // consume comment characters
+                } while ((currentChar != '}') && (currentChar != EOF));
+
+                // Found closing '}'?
+                if (currentChar == '}') {
+                    currentChar = nextChar();  // consume the '}'
+                }
+            }
+            // Not a comment.
+            else {
+                currentChar = nextChar();  // consume whitespace character
+            }
+        }
     }
 }
