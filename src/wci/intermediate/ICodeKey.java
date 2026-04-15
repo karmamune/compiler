d9082ebcb76346319cfa3bc03e7cfeabfe4582f4
karmamune <manugarcia649@gmail.com>
2026-04-15 12:10:53 -0600

    Se añaden las primeras clases y paquetes del capítulo 5 donde se comienza a trabajar en el árbol de análisis

---
+++ src/wci/intermediate/ICodeKey.java
@@ -0,0 +1,10 @@
+package wci.intermediate;
+
+/**
+ * <h1>ICodeKey</h1>
+ * 
+ * <p>The interface for an attribute key of an intermediate code node.</p>
+ */
+public interface ICodeKey {
+
+}
---
+++ src/wci/intermediate/ICodeNodeType.java
@@ -0,0 +1,10 @@
+package wci.intermediate;
+
+/**
+ * <h1>ICodeNodeType</h1>
+ * 
+ * <p>Node types of the intermediate code parse tree.</p>
+ */
+public interface ICodeNodeType {
+
+}
