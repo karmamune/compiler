947cc7ad3d54747d366f6fc3a60d20e291f5c500
karmamune <manugarcia649@gmail.com>
2026-04-18 17:51:36 -0600

    Se implementa todo el código del capítulo 6 donde se trabaja la primera implementación de un ejecutor de código intermedio

--- src/wci/Pascal.java
+++ src/wci/Pascal.java
@@ -9,7 +9,7 @@ import wci.backend.*;
 import wci.message.*;
 import wci.util.*;
 
-import static wci.frontend.pascal.PascalTokenType.STRING;
+// import static wci.frontend.pascal.PascalTokenType.STRING;
 import static wci.message.MessageType.*;
 
 /**
@@ -140,10 +140,10 @@ public class Pascal {
         }
     }
 
-    private static final String TOKEN_FORMAT =
-        ">>> %-15s line=%03d, pos=%2d, text=\"%s\"";
-    private static final String VALUE_FORMAT =
-        ">>>                 value=%s";
+    // private static final String TOKEN_FORMAT =
+    //     ">>> %-15s line=%03d, pos=%2d, text=\"%s\"";
+    // private static final String VALUE_FORMAT =
+    //     ">>>                 value=%s";
 
     private static final String PARSER_SUMMARY_FORMAT =
         "\n%,20d source lines." +
@@ -167,24 +167,34 @@ public class Pascal {
 
             switch (type) {
 
-                case TOKEN: {
-                    Object body[] = (Object []) message.getBody();
-                    int line = (Integer) body[0];
-                    int position = (Integer) body[1];
-                    TokenType tokenType = (TokenType) body[2];
-                    String tokenText = (String) body[3];
-                    Object tokenValue = body[4];
+                // case TOKEN: {
+                //     Object body[] = (Object []) message.getBody();
+                //     int line = (Integer) body[0];
+                //     int position = (Integer) body[1];
+                //     TokenType tokenType = (TokenType) body[2];
+                //     String tokenText = (String) body[3];
+                //     Object tokenValue = body[4];
 
-                    System.out.println(String.format(TOKEN_FORMAT, tokenType, line, position, tokenText));
+                //     System.out.println(String.format(TOKEN_FORMAT, tokenType, line, position, tokenText));
 
-                    if (tokenValue != null) {
-                        if (tokenType == STRING) {
-                            tokenValue = "\"" + tokenValue + "\"";
-                        }
+                //     if (tokenValue != null) {
+                //         if (tokenType == STRING) {
+                //             tokenValue = "\"" + tokenValue + "\"";
+                //         }
 
-                        System.out.println(String.format(VALUE_FORMAT, tokenValue));
-                    }
+                //         System.out.println(String.format(VALUE_FORMAT, tokenValue));
+                //     }
 
+                //     break;
+                // }
+
+                case PARSER_SUMMARY: {
+                    Number body[] = (Number[]) message.getBody();
+                    int statementCount = (Integer) body[0];
+                    int syntaxErrors = (Integer) body[1];
+                    float elapsedTime = (Float) body[2];
+
+                    System.out.printf(PARSER_SUMMARY_FORMAT, statementCount, syntaxErrors, elapsedTime);
                     break;
                 }
 
@@ -214,16 +224,6 @@ public class Pascal {
                     System.out.println(flagBuffer.toString());
                     break;
                 }
-
-                case PARSER_SUMMARY: {
-                    Number body[] = (Number[]) message.getBody();
-                    int statementCount = (Integer) body[0];
-                    int syntaxErrors = (Integer) body[1];
-                    float elapsedTime = (Float) body[2];
-
-                    System.out.printf(PARSER_SUMMARY_FORMAT, statementCount, syntaxErrors, elapsedTime);
-                    break;
-                }
             }
         }
     }
@@ -237,11 +237,19 @@ public class Pascal {
         "\n%,20d instructions generated." +
         "\n%,20.2f seconds total code generation time.\n";
 
+    private static final String LINE_FORMAT =
+        ">>> AT LINE %03d\n";
+
+    private static final String ASSIGN_FORMAT =
+        ">>> LINE %03d: %s = %s\n";
+
     /**
      * Listener for back end messages.
      */
     private class BackendMessageListener implements MessageListener
     {
+        private boolean firstOutputMessage = true;
+
         /**
          * Called by the back end whenever it produces a message.
          * @param message the message.
@@ -251,6 +259,35 @@ public class Pascal {
             MessageType type = message.getType();
 
             switch (type) {
+
+                case ASSIGN: {
+                    if (firstOutputMessage) {
+                        System.out.println("\n===== OUTPUT =====\n");
+                        firstOutputMessage = false;
+                    }
+
+                    Object body[] = (Object[]) message.getBody();
+                    int lineNumber = (Integer) body[0];
+                    String variableName = (String) body[1];
+                    Object value = body[2];
+
+                    System.out.printf(ASSIGN_FORMAT, lineNumber, variableName, value);
+                    break;
+                }
+
+                case RUNTIME_ERROR: {
+                    Object body[] = (Object []) message.getBody();
+                    String errorMessage = (String) body[0];
+                    Integer lineNumber = (Integer) body[1];
+
+                    System.out.print("*** RUNTIME ERROR");
+                    if (lineNumber != null) {
+                        System.out.print(" AT LINE " + String.format("%03d", lineNumber));
+                    }
+                    System.out.println(": " + errorMessage);
+                    break;
+                }
+
                 case INTERPRETER_SUMMARY: {
                     Number body[] = (Number[]) message.getBody();
                     int executionCount = (Integer) body[0];
---
+++ src/wci/backend/interpreter/executors/AssignmentExecutor.java
@@ -0,0 +1,63 @@
+package wci.backend.interpreter.executors;
+
+import java.util.ArrayList;
+
+import wci.intermediate.*;
+import wci.intermediate.icodeimpl.*;
+import wci.backend.interpreter.*;
+import wci.message.*;
+
+import static wci.intermediate.symtabimpl.SymTabKeyImpl.*;
+import static wci.intermediate.icodeimpl.ICodeKeyImpl.*;
+import static wci.message.MessageType.ASSIGN;
+
+/**
+ * <h1>AssignmentExecutor</h1>
+ * 
+ * <p>Execute an assignment statement.</p>
+ */
+public class AssignmentExecutor extends StatementExecutor {
+
+    /**
+     * Constructor.
+     * @param parent the parent executor.
+     */
+    public AssignmentExecutor(Executor parent)
+    {
+        super(parent);
+    }
+
+
+    public Object execute(ICodeNode node)
+    {
+        // The ASSIGN node's children are the target variable
+        // and the expression.
+        ArrayList<ICodeNode> children = node.getChildren();
+        ICodeNode variableNode = children.get(0);
+        ICodeNode expressionNode = children.get(1);
+
+        // Execute the expression and get its value.
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor(this);
+        Object value = expressionExecutor.execute(expressionNode);
+
+        // Set the value as an attribute of the variable's symbol table entry.
+        SymTabEntry variableId = (SymTabEntry) variableNode.getAttribute(ID);
+        variableId.setAttribute(DATA_VALUE, value);
+
+        sendMessage(node, variableId.getName(), value);
+
+        ++executionCount;
+        return null;
+    }
+
+    
+    private void sendMessage(ICodeNode node, String variableName, Object value)
+    {
+        Object lineNumber = node.getAttribute(LINE);
+
+        // Send an ASSIGN message.
+        if (lineNumber != null) {
+            sendMessage(new Message(ASSIGN, new Object[] {lineNumber, variableName, value}));
+        }
+    }
+}
