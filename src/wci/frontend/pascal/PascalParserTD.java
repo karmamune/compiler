37b1110b692fb8548b4a270bdf46f3cca58d9b3d
karmamune <manugarcia649@gmail.com>
2026-04-20 11:39:11 -0600

    Se comienza a trabajar con el capítulo 7, donde se implementarán analizadores para las instrucciones WHILE, REPEAT, FOR, IF y CASE

--- src/wci/frontend/pascal/PascalParserTD.java
+++ src/wci/frontend/pascal/PascalParserTD.java
@@ -1,5 +1,7 @@
 package wci.frontend.pascal;
 
+import java.util.EnumSet;
+
 import wci.frontend.*;
 import wci.frontend.pascal.parsers.*;
 import wci.intermediate.*;
@@ -7,7 +9,7 @@ import wci.message.*;
 
 import static wci.frontend.pascal.PascalTokenType.*;
 import static wci.frontend.pascal.PascalErrorCode.*;
-import static wci.intermediate.symtabimpl.SymTabKeyImpl.*;
+// import static wci.intermediate.symtabimpl.SymTabKeyImpl.*;
 import static wci.message.MessageType.PARSER_SUMMARY;
 
 /**
@@ -100,5 +102,33 @@ public class PascalParserTD extends Parser {
     public int getErrorCount()
     {
         return errorHandler.getErrorCount();
+    }
+
+    /**
+     * Synchronize the parser.
+     * @param syncSet the set of token types for synchronizing the parser.
+     * @return the token where the parser has synchronized.
+     * @throws Exception if an error occurred.
+     */
+    public Token synchronize(EnumSet syncSet)
+        throws Exception
+    {
+        Token token = currentToken();
+
+        // If the current token is not in the synchronization set,
+        // then it is unexpected and the parser must recover.
+        if (!syncSet.contains(token.getType())) {
+
+            // Flag the unexpected token.
+            errorHandler.flag(token, UNEXPECTED_TOKEN, this);
+
+            // Recover by skipping tokens that are not
+            // in the synchronization set.
+            do {
+                token = nextToken();
+            } while (!(token instanceof EofToken) && !syncSet.contains(token.getType()));
+        }
+
+        return token;
     }
 }
